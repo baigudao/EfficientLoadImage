fec2d745456acb6da1fa82c9b0144ccf8e04f3ad
jackie <971060378@qq.com>
2016-12-20 17:49:30 +0800

    高效加载图片，对郭霖的四篇博客进行了整合。 android图片处理（根据郭霖博客）： 1，http://blog.csdn.net/guolin_blog/article/details/9316683 2，http://blog.csdn.net/guolin_blog/article/details/9526203 3，http://blog.csdn.net/guolin_blog/article/details/10470797 4，http://blog.csdn.net/guolin_blog/article/details/11100327

--- app/src/main/java/com/jackie/efficientloadimage/activity/MainActivity.java
+++ app/src/main/java/com/jackie/efficientloadimage/activity/MainActivity.java
@@ -2,7 +2,6 @@ package com.jackie.efficientloadimage.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 import com.jackie.efficientloadimage.R;
 
@@ -11,17 +10,11 @@ import com.jackie.efficientloadimage.R;
  */
 public class MainActivity extends Activity {
 
-    private TextView textView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
-
-        textView = (TextView) findViewById(R.id.tv);
-
-        int maxMemory = (int) (Runtime.getRuntime().maxMemory() / (1024 * 1024));
-        textView.setText("最大内存是：" + maxMemory + "MB");
     }
 }
+
+
